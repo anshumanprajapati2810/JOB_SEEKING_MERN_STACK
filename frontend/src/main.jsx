85fb9ebd7814f6e58ac62c19c3e53b7958ad34c0
anshumanprajapati2810 <22it003@charusat.edu.in>
2024-03-25 12:45:53 +0530

    FE: Added dumy console

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,7 @@ export const Context = createContext({isAuthorized : false});
 const AppWrapper = () => {
   const[isAuthorized, setIsAuthorized] = useState(false);
   const [user, setUser] = useState({});
+  console.log("USER", user)
 return (
   <Context.Provider value={[isAuthorized, setIsAuthorized,user, setUser]}> 
     <App />
