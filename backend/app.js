90e8f9afffc39dc9d36b8765d3828bde18f12c96
anshumanprajapati2810 <22it003@charusat.edu.in>
2024-03-29 22:42:10 +0530

    backedn fix

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const app = express();
 dotenv.config({path:"../.env"})
 
 app.use(cors({
-    origin: "https://job-seeking-mern-stack.vercel.app",
+    origin: "*",
     methods:["GET", "POST", "PUT", "DELETE"],
     credentials:true,   
 },));
