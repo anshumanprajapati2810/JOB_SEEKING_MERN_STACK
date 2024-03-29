c557c6dbe94bf466724438f721107b585e660f8c
anshumanprajapati2810 <22it003@charusat.edu.in>
2024-03-29 22:32:30 +0530

    bug

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,20 +20,20 @@ import { Toaster } from 'react-hot-toast';
 
 const App = () => {
 
-  // const {isAuthorized, setIsAuthorized, setUser } = useContext(Context);
+  const {isAuthorized, setIsAuthorized, setUser } = useContext(Context);
 
-  // useEffect(() => {
-  //   const fetchUser = async () =>{
-  //     try {
-  //       const response = await axios.get("https://job-seeking-mern-stack.onrender.com/api/v1/user/getuser", {withCredentials : true});
-  //       setUser(response.data.user);
-  //       setIsAuthorized(true);
-  //     } catch (error) {
-  //       setIsAuthorized(false);
-  //     }
-  //   };
-  //   fetchUser();
-  // }, [isAuthorized]);
+  useEffect(() => {
+    const fetchUser = async () =>{
+      try {
+        const response = await axios.get("https://job-seeking-mern-stack.onrender.com/api/v1/user/getuser", {withCredentials : true});
+        setUser(response.data.user);
+        setIsAuthorized(true);
+      } catch (error) {
+        setIsAuthorized(false);
+      }
+    };
+    fetchUser();
+  }, [isAuthorized]);
 
   return (
     <>
