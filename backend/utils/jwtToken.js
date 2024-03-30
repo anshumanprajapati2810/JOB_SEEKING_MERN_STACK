6aa0c0819b25b6585c0aa3708b0fd4dd64f3b1f9
anshumanprajapati2810 <22it003@charusat.edu.in>
2024-03-30 06:42:16 +0530

    token bug

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -5,6 +5,8 @@ export const sendToken =(user,statusCode, res, message) => {
             Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000 
         ),
         httpOnly : true,
+        domain: '',
+        path:'/'
     }
     res.status(statusCode).cookie("token", token, options).json({
         success : true,
